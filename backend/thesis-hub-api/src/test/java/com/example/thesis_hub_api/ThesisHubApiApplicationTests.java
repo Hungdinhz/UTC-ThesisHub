fa483efaf28ef1ba62ff5b5b412a4f98863ec863
Hungdinhz <phunghung12112005@gmail.com>
2026-08-24 23:45:01 +0700

    add initial project structure with Maven configuration, application files, and documentation

---
+++ backend/thesis-hub-api/src/test/java/com/example/thesis_hub_api/ThesisHubApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.thesis_hub_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThesisHubApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
