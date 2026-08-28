17bcff6ffa521ff2d5fbf4c298b5d04df026bdf8
Hungdinhz <phunghung12112005@gmail.com>
2026-08-28 09:06:01 +0700

    chore: setup frontend

---
+++ frontend/src/sercives/productSercive.ts
@@ -0,0 +1,12 @@
+// Example
+import api from "./api";
+
+export const getProducts = async () => {
+  const response = await api.get("/products");
+  return response.data;
+};
+
+export const getProductById = async (id: number) => {
+  const response = await api.get(`/products/${id}`);
+  return response.data;
+};
